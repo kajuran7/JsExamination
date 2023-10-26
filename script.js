ffed256ba19530017baa17a0591569c6c3455ed4
kajuran7 <kajubass@gmail.com>
2023-10-26 10:45:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,109 @@
+// Question No1 Done
+//Question No2
+
+// var person = {
+//     name: "Kajuran",
+//     age: 23,
+//  email:"john@example.com"
+//     // introduceYou: function(){console.log("Hello, my name is " + this.name);}
+// }
+
+// person.introduceYou();
+//  var person = { kajuran
+//     name: "kajuran",
+//     greetstudent: function(){console.log ("Hello + this.name")}
+
+//  }
+//  function greet(greetstudent) {
+//     return "Hello "+person.name+" welcome to the codig school"
+// }
+// console.log(greet("greetstudent"));
+
+// console.log("Using dot notation:");
+// console.log("Name: " + person.name);
+// console.log("age: " + person.age);
+
+// console.log("Using bracket notation:");
+// console.log("Name: " + person["name"]);
+// console.log("age: " + person["age"]);
+// console.log("email: " + person ["email"]);
+// delete person .email;
+// console.log(person);
+
+
+// // Check if "email" property is defined// Q7
+// if (person.email !== undefined) {
+//     console.log("The 'email' property exists in the object.");
+// } else {
+//     console.log("The 'email' property does not exist in the object.");
+// }
+
+
+
+
+
+// Q08 create two object student and course and merge them into a new object named student course 
+// Q09 How do you check if an object  has a specific property, say , address? If there an address, log (“adress is there”) in the console if not (“Address not found”)?
+//Q10 create an object book with propper for title author and year and the convert it into json string 
+//Q11 Create an array of objects called "students" Each object in the array should have the following properties 
+// name:string 
+// age:number 
+// gps:number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var students = [
+//     {
+//         name: "Alice",
+//         age: 20,
+//         grade: "A"
+//     },
+//     {
+//         name: "Bob",
+//         age: 22,
+//         grade: "B"
+//     },
+//     {
+//         name: "Charlie",
+//         age: 19,
+//         grade: "A-"
+//     }
+// ];
+
+// // You can access individual students and their properties like this:
+// console.log(students[0].name); // Alice
+// console.log(students[1].age);  // 22
+// console.log(students[2].grade); // A-
